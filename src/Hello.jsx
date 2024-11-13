abcb1effa823f5732dd3fe35bd2d0fcd9eff9b71
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-13 22:06:59 +0700

    Added some components.
    Container() is used to hold everything.
    Button() changes its color when clicked/unclicked and has a counter.

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,5 +1,52 @@
+import { useState } from 'react'
+import './Hello.css';
+
 function Hello() {
-    return <h1>Hello World!</h1>;
+    return (
+        <div className="box">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
-export default Hello;
+function Button() {
+    const [index, setIndex] = useState(0);
+    const [mouseDown, setMouseDown] = useState(false);
+
+    function click() {
+        setIndex(index + 1);
+        setMouseDown(true);
+    }
+
+    function unclick() {
+        setMouseDown(false);
+    }
+
+    const buttonStyle = {
+        border: mouseDown ? 'thick double maroon' : 'thick double slateblue',
+    }
+
+    const numStyle = {
+        transition: 'color 0.5s ease',
+        color: mouseDown ? 'red' : 'black',
+    }
+
+    return (
+        <button id="button" className="box" style={buttonStyle} onMouseDown={click} onMouseUp={unclick}>
+            <h1>Hi: <span style={numStyle}>{index}</span></h1>
+        </button>
+    );
+}
+
+function Container() {
+    return (
+        <div id="container">
+            <Hello />
+            <Hello />
+            <Hello />
+            <Button />
+        </div>
+    );
+}
+
+export { Container };
